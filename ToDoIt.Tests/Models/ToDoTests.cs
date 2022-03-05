c0867ffa72ceb7ca22d8cc6346664097ff435196
fsojudi@gmail.com <fsojudi@gmail.com>
2022-03-05 15:18:51 +0100

    Part 4

---
+++ ToDoIt.Tests/Models/ToDoTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ToDoIt_Assignment.Models;
+
+
+namespace ToDoIt_Assignment.Tests.Models
+{
+    public class ToDoTests
+    {
+        [Fact]
+        public void ToDoConstructorTest()
+        { 
+            int id = 2;
+            string discription = "Good";
+            string firstName = "Fatemeh";
+            string lastName = "Sojudi";
+            Person assignee = new Person(id, firstName, lastName);
+           
+            ToDo result = new ToDo(id, discription,assignee );
+            Assert.NotNull(result);
+            Assert.Equal(id, result.Id);
+            Assert.Equal(discription, result.Description);
+            Assert.Equal(assignee, result.Assignee);
+
+        }
+
+        [Fact]
+        public void GoodDiscriptionTest()
+        {
+            int id = 1;
+            String discription = "GoodDiscriptionWorks";
+            string firstName = "Fatemeh";
+            string lastName = "Sojudi";
+            Person assignee = new Person(id, firstName, lastName);
+
+            ToDo result = new ToDo(id, discription,assignee );
+
+            Assert.Equal(discription, result.Description);
+
+
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void BadDiscriptionTest(string badDicription)
+        {
+            int id = 3;
+            string firstName = "Fatemeh";
+            string lastName = "Sojudi";
+            Person assignee = new Person(id, firstName, lastName);
+            ToDo todo = new ToDo(id, badDicription,assignee );
+
+            Assert.Throws<ArgumentException>(() => todo.Description = badDicription);
+
+
+        }
+
+        [Fact]
+        public void GoodIdTest()
+        {
+            int id = 1;
+            String discription = "Good";
+            string firstName = "Fatemeh";
+            string lastName = "Sojudi";
+            Person assignee = new Person(id, firstName, lastName);
+
+            ToDo result = new ToDo(id, discription,assignee );
+
+            Assert.Equal(id, result.Id);
+
+
+        }
+
+        [Fact]
+        public void GoodAssigneeTest()
+        {
+            int id = 1;
+            string firstName = "Fatemeh";
+            string lastName = "Sojudi";
+            string description = "Good";
+            Person GoodAssigneeWorks = new Person(id, firstName, lastName);
+
+            ToDo result = new ToDo(id, description, GoodAssigneeWorks);
+
+            Assert.Equal(GoodAssigneeWorks, result.Assignee);
+
+        }
+        [Fact]
+        public void GoodDoneTest()
+        {
+            int id = 1;
+            String discription = "Good";
+            bool DoneWorks = false;
+            string firstName = "Fatemeh";
+            string lastName = "Sojudi";
+            Person assignee = new Person(id, firstName, lastName);
+
+
+            ToDo result = new ToDo(id, discription,assignee );
+
+            Assert.Equal(DoneWorks, result.Done);
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
--- ToDoIt/Models/ToDo.cs
+++ ToDoIt/Models/ToDo.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Text;
+using ToDoIt_Assignment.Models;
 
-namespace ToDoIt.Models
+namespace ToDoIt_Assignment.Models
 {
-    class ToDo
+    public class ToDo
     {
+        private int id;
+        private string description;
+        private bool done;
+        Person assignee;
+
+        public ToDo(int id, string description, Person assignee)
+        {
+            this.id = id;
+            this.description = description;
+            this.assignee = assignee;
+
+        }
+        public int Id
+        {
+            get { return id; }
+
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Discription Can not be empty .");
+                }
+                else
+                {
+                    description = value;
+                }
+            }
+        }
+        public Person Assignee
+        {
+            get { return assignee; }
+            set { assignee = value; }
+        }
+        public bool Done
+        {
+            get { return done; }
+            set { done = value; }
+        }
+
+
+
+
+
+
     }
 }
