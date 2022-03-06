79e26fd78b2235cdd14969baa16551b25627264b
fsojudi@gmail.com <fsojudi@gmail.com>
2022-03-06 21:26:59 +0100

    uppdating Assignment

---
+++ ToDoIt.Tests/Data/TodoSequencerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ToDoIt.Data;
+
+
+namespace ToDoIt.Tests.Data
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void NextTodoIdTest()
+        {
+            int Id = 1;
+
+
+            Assert.Equal(2, TodoSequencer.NextTodoId(Id));
+
+        }
+        [Fact]
+        public void ResetTest()
+        {
+
+            int Id = 3;
+            int resetWorks = TodoSequencer.Reset(Id);
+
+            Assert.Equal(0, resetWorks);
+
+        }
+
+
+    }
+}
---
+++ ToDoIt/Data/TodoSequencer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIt.Data
+{
+    public class TodoSequencer
+    {
+        private static int todoId;
+
+        public TodoSequencer(int TodoId)
+        {
+            TodoId = todoId;
+        }
+
+        public int Id
+        {
+            get { return todoId ; }//get/read
+        }
+        public static int NextTodoId(int NextTodoId)
+        {
+
+            return ++(NextTodoId);
+
+
+        }
+        public static int Reset(int resetTodoId)
+        {
+            resetTodoId = 0;
+            return resetTodoId;
+
+        }
+
+
+
+    }
+}
